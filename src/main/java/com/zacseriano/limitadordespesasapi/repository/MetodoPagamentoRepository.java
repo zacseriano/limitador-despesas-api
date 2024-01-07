cc201726acabf3cc8e1862406fcaecedeac0b8b1
Zacarias Seriano Jr. <zacarias.junior@maida.health>
2024-01-07 00:26:31 -0300

    [feature] adicionando repositories

---
+++ src/main/java/com/zacseriano/limitadordespesasapi/repository/MetodoPagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.zacseriano.limitadordespesasapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.zacseriano.limitadordespesasapi.domain.model.MetodoPagamento;
+
+public interface MetodoPagamentoRepository extends JpaRepository<MetodoPagamento, Long>, JpaSpecificationExecutor<MetodoPagamento>{
+
+}
---
+++ src/main/java/com/zacseriano/limitadordespesasapi/repository/TipoDespesaRepository.java
@@ -0,0 +1,10 @@
+package com.zacseriano.limitadordespesasapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.zacseriano.limitadordespesasapi.domain.model.Despesa;
+
+public interface TipoDespesaRepository extends JpaRepository<Despesa, Long>, JpaSpecificationExecutor<Despesa> {
+
+}
